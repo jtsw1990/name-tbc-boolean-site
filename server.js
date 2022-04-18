02afc18db6164f22fce775097fdb42cc4ba0650d
jtsw1990 <jtsw1990@gmail.com>
2022-04-18 14:02:25 +1000

    updated naming

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ let ipinfo = new IPinfoWrapper(process.env.ipinfoToken);
 
 // Middleware and static file config
 app.set("view engine", "ejs");
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride("_method"))
 
